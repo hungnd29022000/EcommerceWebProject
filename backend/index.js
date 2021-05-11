bb0b21365e91c2607e68e5624c242c1ee242f51e
boyonepiece <stah432987@gmail.com>
2021-05-11 14:53:47 +0700

    them api brand

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 let multer = require('multer');
 var Product = require('./routes/product');
 var Wishlist = require('./routes/wishlist');
+var Brand = require('./routes/brand')
 
 var app = express();
 let upload = new multer()
@@ -20,6 +21,7 @@ app.use('/products',Product);
 // for parsing multipart/form-data
 app.use(upload.array())
 app.use('/wishlist', Wishlist);
+app.use('/brand',Brand)
 
 const port = process.env.PORT || 4000 // export PORT = 4000
 app.listen(port,()=>{
