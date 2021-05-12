eb49ce2406fc411baaf40c1c83d5a81149b13bb4
boyonepiece <stah432987@gmail.com>
2021-05-12 10:10:23 +0700

    fix conflict

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,7 @@ let multer = require('multer');
 var Product = require('./routes/product');
 var Wishlist = require('./routes/wishlist');
 var Brand = require('./routes/brand')
-<<<<<<< HEAD
-=======
 var Customer = require('./routes/customer')
->>>>>>> a8c0359d8cc3d11ef505fe8c5b210aaa693adb7e
 
 var app = express();
 let upload = new multer()
@@ -26,10 +23,7 @@ app.use('/products',Product);
 app.use(upload.array())
 app.use('/wishlist', Wishlist);
 app.use('/brand',Brand)
-<<<<<<< HEAD
-=======
 app.use('/customer',Customer)
->>>>>>> a8c0359d8cc3d11ef505fe8c5b210aaa693adb7e
 
 const port = process.env.PORT || 4000 // export PORT = 4000
 app.listen(port,()=>{
