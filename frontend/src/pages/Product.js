c10f9711b4c6cd5b7018ebaab4bdafff45d1230e
dai02012000 <ledai02012000@gmail.com>
2021-05-23 08:25:00 +0700

    add product

---
+++ frontend/src/pages/Product.js
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from "react";
+import Header from "../components/Header/Header";
+import RecommendBanner from "../components/RecommendBanner/RecommendBanner";
+import Footer from "../components/Footer/Footer";
+import Newsletter from "../components/NewsLetter/NewsLetter";
+import ProductFilter from "../components/Filter/ProductFilter";
+import Banner from "../components/Banner/Banner";
+export default function Product(){
+    const [collection, setCollection] = useState([])
+    useEffect(() => {
+        document.body.style.overflow = 'unset'; // chua biet de lam gi
+    }, [])
+    return (
+        <div className="">
+            <Header/>
+            <Banner collection = {collection}/>
+            <RecommendBanner/>
+            <ProductFilter/>
+            <Newsletter/>
+            <Footer/>
+        </div>
+    )
+}
